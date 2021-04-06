a0cb791cde9536cbfadc9dccf1bb09189da8b035
Victor Meireles <victordinami@gmail.com>
2021-04-06 12:40:01 -0300

    Correções cookie visitanteId

--- src/config/visitanteCheck.js
+++ src/config/visitanteCheck.js
@@ -2,12 +2,14 @@ import Cookies from 'js-cookie';
 import axios from './axios';
 
 async function visitanteCheck() {
+  // const dominio = '.localhost.local'; // DEV
+  const dominio = '.boicote.app';
   const visitanteId = Cookies.get('visitanteId');
 
   if (visitanteId === undefined) {
     try {
       const response = await axios.get('/visitantes/novo-visitante', { withCredentials: false });
-      Cookies.set('visitanteId', response.data, { expires: 1825 });
+      Cookies.set('visitanteId', response.data, { path: '', domain: dominio, expires: 1825 });
     } catch (err) {
       // console.error(err);
     }
